2fc985d8a57a246fe34bedce21e4de9d77b99456
jonrim <jonrim@umich.edu>
2015-01-21 22:17:53 -0500

    .

---
+++ Super Mario Bros. 3/Assets/Sound/Finish.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finish : MonoBehaviour {
+	public AudioClip Sound;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		audio.PlayOneShot (Sound);
+		Time.timeScale = 0;
+	}
+}
